d3ae1777bc097eefaa611fb90d46d31386fc2b08
oceaquaris <oceaquarisoxyheloris@gmail.com>
2014-11-20 21:00:09 -0500

    Documentation!

--- dios/opcontrol.c
+++ dios/opcontrol.c
@@ -1,16 +1,20 @@
-/******************* *****************************************************/
-/* @file opcontrol.c													*/
-/* @brief Source file for operator control.								*/
-/* Copyright (c) 2014-2015 Olympic Steel Eagles. All rights reserved.	*/
-/* Portions of this file may contain elements from the PROS API.		*/
-/* See include/API.h for additional notice.								*/
-/************************************************************************/
+/**
+ * @file opcontrol.c
+ * @brief Source file for operator control.
+ *
+ * Copyright (c) 2014-2015 Olympic Steel Eagles. All rights reserved.
+ * Portions of this file may contain elements from the PROS API.
+ * See include/API.h for additional notice.
+ ************************************************************************/
 
 #include "main.h"
 #include "dios/Chassis.h"
 #include "dios/Lift.h"
 #include <math.h>
 
+/**
+ * @brief Sets motors in motion based on user input (from controls).
+ */
 void operatorControl()
 {
 	while (true)

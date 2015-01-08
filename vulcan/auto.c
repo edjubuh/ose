1e1fee6f90183c0509503b6b41aebb9d4328eae0
Elliot Berman <elliot@elliotjb.com>
2015-01-08 16:29:29 -0500

    Merged Dios to Vulcan for lift control.

--- vulcan/auto.c
+++ vulcan/auto.c
@@ -1,18 +1,21 @@
-/************************************************************************/
-/* @file auto.c		@brief Source file for autonomous functions.		*/
-/*														                */
-/* Copyright (c) 2014-2015 Olympic Steel Eagles. All rights reserved.	*/
-/* Portions of this file may contain elements from the PROS API.		*/
-/* See include/API.h for additional notice.								*/
-/************************************************************************/
+/**
+ * @file auto.c
+ * @brief Source file for autonomous functions.
+ *
+ * Copyright (c) 2014-2015 Olympic Steel Eagles. All rights reserved.
+ * Portions of this file may contain elements from the PROS API.
+ * See include/API.h for additional notice.
+ ************************************************************************/
 
 #include "main.h"
-#include "vulcan/Chassis.h"
+#include "dios/Chassis.h"
+#include "dios/Lift.h"
 
+
+/**
+ * @brief The autonomous function executed during autonomous mode.
+ */
 void autonomous()
 {
-	lcdSetText(uart1, 2, "HULK SMASH");
-	ChassisSet(-127, -127, false);
-	delay(4000);
-	ChassisSet(0, 0, false);
+	
 }

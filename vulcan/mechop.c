e1e9ff2869b2bed1cab5decb41b3d93e0878de11
None <rs14@alpha.omega>
2015-01-17 12:17:33 -0500

    Fixed thetaSector() in tank/mechanum drive hybrid & made the function more efficient.

--- vulcan/mechop.c
+++ vulcan/mechop.c
@@ -1,11 +1,11 @@
 /**
-* @file dios/mechop.c
-* @brief Source file for mecanum functions
-*
-* Copyright (c) 2014-2015 Olympic Steel Eagles. All rights reserved.
-* Portions of this file may contain elements from the PROS API.
-* See include/API.h for additional notice.
-***********************************************************************/
+ * @file vulcan/mechop.c
+ * @brief Source file for mecanum functions
+ *
+ * Copyright (c) 2014-2015 Olympic Steel Eagles. All rights reserved.
+ * Portions of this file may contain elements from the PROS API.
+ * See include/API.h for additional notice.
+ ***********************************************************************/
 
 #include <math.h>
 #include "main.h"
@@ -15,26 +15,12 @@
 
 
 /**
- * @brief Functions for converting radians to degrees
- * NOTE: M_PI is defined in math.h
- *
- * @param radians
- *        radian value of the angle to be converted
- *
- * @return Degree value (as a double or as an int: depends on the function)
- */
-double toDegrees(double radians)
-{
-	return (radians * (180/M_PI));
-}
-int toIntDegrees(double radians)
-{
-	return (int)(radians * (180/M_PI));
-}
-
-
-/**
  * @brief Returns theta, determined from function parameters.
+ *
+ * NOTE: Constants defined in math.h
+ *       M_PI     pi constant
+ *       M_PI_2   pi/2 constant
+ *       M_PI_4   pi/4 constant
  *
  * @param x
  *        X value for a joystick.
@@ -45,63 +31,45 @@ int toIntDegrees(double radians)
  */
 double getJoyTheta(int x, int y)
 {
-	if (x == 0)
-	{
-		if (y >= 0)
-			return 90.0;
-		else
-			return -90.0;
-	}
-	else if (x < 0)
-	{
-		if ( atan( ((double)y)/x) < 0.0)
-			return ( toDegrees( atan( ((double)y)/x)) + 180.0 );
-		else
-			return ( toDegrees( atan( ((double)y)/x)) - 180.0 );
-	}
-	else
-		return ( toDegrees( atan( ((double)y)/x) ) );
+    if (x == 0)
+    {
+        if (y >= 0)
+            return M_PI_2;
+        else
+            return -M_PI_2;
+    }
+    else if (x < 0)
+    {
+        if ( atan( ((double)y)/x) < 0.0)
+            return ( atan( ((double)y)/x)) + M_PI );
+        else
+            return ( atan( ((double)y)/x)) - M_PI );
+    }
+    else
+        return ( atan( ((double)y)/x) ) );
 }
 
 
 
 /**
- * @brief Rounds theta values to one value based on what sector theta is in.
- *        Is in degrees in order to simplify comparisons.
+ * @brief Rounds theta values and calculates sector theta is in.
  *
  * @param theta
- *        Angle to be rounded (in degrees!) and its sector determined.
- *        NOTE: The double 'theta' will be rounded to two decimal places and converted to
+ *        Angle to be rounded (in radians) and its sector determined.
+ *        NOTE: The double 'theta' will be rounded to three decimal places and converted to
  *              an int in order to prevent error in comparisons.
- *              EXAMPLE: double 24.526312  ------> int 2453
+ *              EXAMPLE: double 1.526312 r  ------> int 1526
  *
- * @return Sector number 0, 45, 90, 135, 180, -45, -90, -135; & -1 for error <br>
- *  \|/ <br>
- * --0--<br>
- *- /|\ <br>
+ * @return Sector number +/-0, +/-1, +/-2, +/-3, +/-4, +/-5, +/-6, +/-7
+ *  \|/
+ * --0--
+ *- /|\
  */
 int thetaSector( double theta )
 {
-	//get rid of that quirky decimal place!
-	int ltheta = (int)( roundf(theta * 100) );
-	if (ltheta >= -2250 && ltheta <= 2250)
-		return 0;
-	else if (ltheta >= -6750  && ltheta < -2250 )
-		return -45;
-	else if (ltheta >= -11250 && ltheta < -6750 )
-		return -90;
-	else if (ltheta >= -15750 && ltheta < -11250 )
-		return -135;
-	else if ( (ltheta >= -180 && ltheta < -15750 ) || (ltheta > 15750 && ltheta <= 180) )
-		return 180;
-	else if (ltheta > 2250 && ltheta <= 6750 )
-		return 45;
-	else if (ltheta > 6750 && ltheta <= 11250 )
-		return 90;
-	else if (ltheta > 11250 && ltheta <= 15750 )
-		return 135;
-	else
-		return -1;
+    //get rid of that quirky decimal place!
+    //integer representation of theta (in radians) and modulus by 
+    return ((int)( roundf(theta * 1000) )) / 393;
 }
 
 /**
@@ -116,23 +84,23 @@ int thetaSector( double theta )
  */
 int aJoy(int val1, int val2)
 {
-	return (abs(val1) + abs(val2)) / 2;
+    return (abs(val1) + abs(val2)) / 2;
 }
 
 /**
  * @brief Calculates the hypotenuse of a triangle with legs x and y.
  *
  * @param x
- *        first leg of the triangle (or an joystick x value)
+ *        first leg of the triangle (or a joystick x value)
  * @param y
- *        second leg of the triangle (or an joystick y value)
+ *        second leg of the triangle (or a joystick y value)
  *
  * @return The hypotenuse of the triangle (as a double & unrounded).
  */
 double cHypo(int x, int y)
 {
-	return sqrt( (x * x) + (y * y) );
-	//use x*x instead of pow(x, 2) to decrease computation time.
+    return sqrt( (x * x) + (y * y) );
+    //use x*x instead of pow(x, 2) to decrease computation time.
 }
 
 /**
@@ -147,7 +115,7 @@ double cHypo(int x, int y)
  */
 int aHypo(double h1, double h2)
 {
-	return (int)(roundf((h1 + h2) / 2));
+    return (int)(roundf((h1 + h2) / 2));
 }
 
 /**
@@ -155,53 +123,92 @@ int aHypo(double h1, double h2)
  */
 void JoystickControl()
 {
-	int l3 = joystickGetAnalog(1, 3), //up left y
-	    l4 = joystickGetAnalog(1, 4), //side left x
-	    r1 = joystickGetAnalog(1, 1), //side right x
-	    r2 = joystickGetAnalog(1, 2); //up right y
-	int theta1 = thetaSector( getJoyTheta(l4, l3) ),
-	    theta2 = thetaSector( getJoyTheta(r1, r2) );
+    int l3 = joystickGetAnalog(1, 3), //up left y
+        l4 = joystickGetAnalog(1, 4), //side left x
+        r1 = joystickGetAnalog(1, 1), //side right x
+        r2 = joystickGetAnalog(1, 2); //up right y
+    int left = thetaSector( getJoyTheta(l4, l3) ), //left joystick
+        right = thetaSector( getJoyTheta(r1, r2) ); // right joystick
 
-	//Up / Down
-	if (theta1 == 90 && theta2 == 90)
-		ChassisSet( aJoy(r1, l4), aJoy(r1, l4), false);
-	else if (theta1 == -90 && theta2 == -90)
-		ChassisSet( -aJoy(r1, l4), -aJoy(r1, l4), false);
-
-	//Left / Right
-	else if (theta1 == 0 && theta2 == 0)
-		ChassisSetMecanum(-M_PI/2, aJoy(l4, r1), 0, false);
-	else if (theta1 == 180 && theta2 == 180)
-		ChassisSetMecanum(M_PI/2, aJoy(l4, r1), 0, false);
-
-	//Tank Drive ^+v or v+^
-	else if (theta1 == 90 && theta2 == -90)
-		ChassisSet( aJoy(r1, l4), -aJoy(r1, l4), false);
-	else if (theta1 == -90 && theta2 == 90)
-		ChassisSet( -aJoy(r1, l4), aJoy(r1, l4), false);
-
-	//northeast / northwest
-	else if (theta1 == 45 && theta2 == 45)
-		ChassisSetMecanum(-M_PI/4, aHypo( cHypo(l4, l3), cHypo(r1, r2) ), 0, false);
-	else if (theta1 == 135 && theta2 == 135)
-		ChassisSetMecanum(M_PI/4, aHypo( cHypo(l4, l3), cHypo(r1, r2) ), 0, false);
-
-	//southeast / southwest
-	else if (theta1 == -45 && theta2 == -45)
-		ChassisSetMecanum(-3 * M_PI/4, aHypo( cHypo(l4, l3), cHypo(r1, r2) ), 0, false);
-	else if (theta1 == -135 && theta2 == -135)
-		ChassisSetMecanum(3 * M_PI/4, aHypo( cHypo(l4, l3), cHypo(r1, r2) ), 0, false);
-
-	//strafe right / left
-	else if ((theta1 == 90 || theta1 == -90) && theta2 == 0)
-		ChassisSet(l3, (int)(l3 * (1.0 - abs( ((double)r1) / STRAFE_CONST ) ) ), false);
-	else if (theta1 == 180 && (theta2 == 90 || theta2 == -90))
-		ChassisSet( (int)(r2 * (1.0 - abs( ((double)l4) / STRAFE_CONST ) ) ), r2, false);
+    //Up / Down
+    if ( (left  == 3 || left  == 4) &&
+         (right == 3 || right == 4)    )
+        ChassisSet( aJoy(r1, l4),
+                    aJoy(r1, l4),
+                    false);
+    else if ( (left  == -3 || left  == -4) &&
+              (right == -3 || right == -4)    )
+        ChassisSet( -aJoy(r1, l4),
+                    -aJoy(r1, l4),
+                    false);
 
 
-	//TODO: Strafe up / down
+    //Left / Right
+    else if ( (left  == 0 || left  == -0) &&
+              (right == 0 || right == -0)    )
+        ChassisSetMecanum(-M_PI_2, aJoy(l4, r1), 0, false);
+    else if ( (left  == 7 || left  == -7) &&
+              (right == 7 || right == -7)    )
+        ChassisSetMecanum(M_PI_2, 
+                          aJoy(l4, r1), 
+                          0, false);
 
-	//By default: stop, so motors don't break
-	else
-		ChassisSet(0, 0, false);
+
+    //Tank Drive ^+v or v+^
+    else if ( (left  == 3 || left  == 4) &&
+              (right == -3 || right == -4)    )
+        ChassisSet( aJoy(r1, l4),
+                    -aJoy(r1, l4),
+                    false);
+    else if ( (left  == -3 || left  == -4) &&
+              (right == 3  || right == 4)    )
+        ChassisSet( -aJoy(r1, l4),
+                    aJoy(r1, l4),
+                    false);
+
+
+    //northeast / northwest
+    else if ( (left  == 2 || left  == 3) &&
+              (right == 2 || right == 3)    )
+        ChassisSetMecanum(-M_PI_4,
+                          aHypo( cHypo(l4, l3), cHypo(r1, r2) ),
+                          0, false);
+    else if ( (left  == 5 || left  == 6) &&
+              (right == 5 || right == 6)    )
+        ChassisSetMecanum(M_PI_4, 
+                          aHypo( cHypo(l4, l3), cHypo(r1, r2) ),
+                          0, false);
+
+
+    //southeast / southwest
+    else if ( (left  == -2 || left  == -3) &&
+              (right == -2 || right == -3)    )
+        ChassisSetMecanum(-3.0 * M_PI_4,
+                          aHypo( cHypo(l4, l3), cHypo(r1, r2) ),
+                          0, false);
+    else if ( (left  == -5 || left  == -6) &&
+              (right == -5 || right == -6)    )
+        ChassisSetMecanum(3.0 * M_PI_4,
+                          aHypo( cHypo(l4, l3), cHypo(r1, r2) ),
+                          0, false);
+
+
+    //strafe right / left
+    else if ( ( (left == 3 || left == 4) || (left == -3 || left == -4) ) &&
+              (right == 0 || right == -0)                                    )
+        ChassisSet(l3,
+                   (int)(l3 * (1.0 - abs( ((double)r1) / STRAFE_CONST ) ) ),
+                   false);
+    else if ( (left == 7 || left == -7) && 
+              ( (right == 3 || right == 4) || (right == -3 || right == -4) ) )
+        ChassisSet( (int)(r2 * (1.0 - abs( ((double)l4) / STRAFE_CONST ) ) ),
+                    r2,
+                    false);
+
+
+    //TODO: Strafe up / down
+
+    //By default: stop, so motors don't break
+    else
+        ChassisSet(0, 0, false);
 }
